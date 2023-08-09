2bb0ee5f284639ffee932bd0176cbba31ea1459c
StarSmileQA <mihail.russkih@star-smile.ru>
2023-08-09 18:25:11 +0300

    09/08 until 6.2.2

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,6 +1,9 @@
 package base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -12,7 +15,10 @@ public class BaseTest {
 
     @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp(@Optional("chrome") String browser) {
+    public void setUp(@Optional("chrome") String browser, ITestContext ctx) {
+        String testName = ctx.getCurrentXmlTest().getName();
+        log = LogManager.getLogger(testName);
+
         BrowserDriverFactory factory = new BrowserDriverFactory(browser);
         driver = factory.createDriver();
     }
