b4ed48327558d7251751bb3aae7e3f399ed362a1
Mikhail Russkikh <mnrusskikh4@gmail.com>
2023-09-21 15:54:39 +0300

    21.09. Завершенная версия 1.0

--- src/main/java/pages/AuthPageObject.java
+++ src/main/java/pages/AuthPageObject.java
@@ -15,20 +15,19 @@ public class AuthPageObject extends BasePageObject {
 
     private By usernameLocator = By.id("username");
     private By passwordLocator = By.id("password");
-    private By logInButtonLocator = By.id("login-btn");
+    // Исправлено на By.xpath
+    private By logInButtonLocator = By.xpath("//*[@id='app']/div[3]/div/div[1]");
 
     public AuthPageObject(WebDriver driver, Logger log) {
         super(driver, log);
     }
 
-    /** Open AuthPage with it's url */
     public void openPage() {
         log.info("Opening page: " + pageUrl);
         openUrl(pageUrl);
         log.info("Page opened!");
     }
 
-    /** Execute log in */
     public DoctorsAccountPage logIn(String username, String password) {
         log.info("Executing LogIn with username [{}] and password [{}]", username, password);
         type(username, usernameLocator);
@@ -37,12 +36,16 @@ public class AuthPageObject extends BasePageObject {
         return new DoctorsAccountPage(driver, log);
     }
 
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Максимальное ожидание в течение 10 секунд
-    By xpathSelector = By.xpath("//div[@class='v-snack__wrapper v-sheet theme--dark' and contains(@style, 'display: none;')]//div[@class='v-snack__content' and text()='Неверный логин или пароль']");
-    WebElement errorMessageElement = wait.until(ExpectedConditions.presenceOfElementLocated(xpathSelector));
+    // Исправлено на локальную переменную для WebDriverWait
+    public WebElement getErrorMessageElement() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        By xpathSelector = By.xpath("//div[@class='v-snack__wrapper v-sheet theme--dark' and contains(@style, 'display: none;')]//div[@class='v-snack__content' and text()='Неверный логин или пароль']");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(xpathSelector));
+    }
 
+    private By message = By.xpath("//*[@id='app']/div[3]/div/div[1]");
 
     public String getErrorMessageText() {
+        return driver.findElement(message).getText();
     }
 }
-
--- src/test/java/tests/authpagetests/NegativeAuthTests.java
+++ src/test/java/tests/authpagetests/NegativeAuthTests.java
@@ -1,6 +1,5 @@
 package tests.authpagetests;
 
-
 import base.CsvDataProviders;
 import base.TestUtilities;
 import org.openqa.selenium.By;
@@ -16,47 +15,37 @@ import java.util.Map;
 
 public class NegativeAuthTests extends TestUtilities {
 
-
     @Test(priority = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
-    public void negativeAuthTest( Map<String, String> testData) {
+    public void negativeAuthTest(Map<String, String> testData) {
         log.info("Starting negativeTest");
-        //Data
+
         String no = testData.get("no");
         String username = testData.get("username");
         String password = testData.get("password");
         String expectedMessage = testData.get("expectedMessage");
         String description = testData.get("description");
 
-        // open main page
         AuthPageObject authPage = new AuthPageObject(driver, log);
         authPage.openPage();
 
-        // enter username and password
-        // Проверка на пустые значения username и password
         if (username == null || username.isEmpty()) {
-            // Вывод ошибки или выполнение других действий по вашему усмотрению
             System.out.println("Ошибка: Поле 'username' пустое или null");
         } else {
             driver.findElement(By.id("username")).sendKeys(username);
         }
 
         if (password == null || password.isEmpty()) {
-            // Вывод ошибки или выполнение других действий по вашему усмотрению
             System.out.println("Ошибка: Поле 'password' пустое или null");
         } else {
             driver.findElement(By.id("password")).sendKeys(password);
         }
 
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        // push log in button
         WebElement loginButton = driver.findElement(By.id("login-btn"));
         wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         loginButton.click();
         takeScreenshot("Login button pushed");
 
-        // Verification
         String expectedErrorMessage = "Неверный логин или пароль";
         String authErrorMessage = authPage.getErrorMessageText();
         Assert.assertTrue(authErrorMessage.contains(expectedErrorMessage),
